4fa7521607b6d850f1ce9ef8d33903a5ca4e2607
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-08-29 02:44:39 -0300

    🎨 Final layout for landing view

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,16 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <meta name="description" content="{{ config('app.description') }}">
-    <meta name="keyword" content="{{ config('app.keyword') }}">
+    <meta name="keywords" content="{{ config('app.keyword') }}">
     <meta name="author" content="{{ config('app.author') }}">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -19,18 +16,21 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Styles -->
     <link href="{{ mix('css/practice_therapy.css') }}" rel="stylesheet">
     <link href="{{ mix('css/theme.css') }}" rel="stylesheet">
 </head>
 
 <body>
-
     <noscript>
         <strong>
             {{ __("We're sorry but Practice Therapy doesn't work properly without JavaScript enabled. Please enable it and refresh the page.") }}
         </strong>
     </noscript>
+
     <div id="PracticeTherapy">
         <practicetherapy></practicetherapy>
     </div>
