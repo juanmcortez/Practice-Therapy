d7b8cc8085401ef37e1effa7256b14a4f49f9a31
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-08-31 03:49:01 -0300

    👷Temporay api response

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('patients/list', function () {
+    $tmppatient = [
+        ['pid' => 0, 'p_name' => 'Cortéz, Juan M.', 'p_dob' => '1980 - 04 - 08', 'p_extpid' => 156849],
+        ['pid' => 1, 'p_name' => 'Carnizzaro, Vanesa A.', 'p_dob' => '1984 - 04 - 13', 'p_extpid' => 156851],
+        ['pid' => 2, 'p_name' => 'Cortéz Carnizzaro, María T.', 'p_dob' => '2009 - 09 - 26', 'p_extpid' => 156855],
+    ];
+    return json_encode($tmppatient);
+});
