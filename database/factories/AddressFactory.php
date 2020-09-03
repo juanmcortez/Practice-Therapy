8f91bca2a712efa37aef1d25828038449b862a69
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-09-03 02:52:08 -0300

    🗃 Patient Address Model

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Http\Models\Patients\Address;
+use Faker\Generator as Faker;
+
+$factory->define(Address::class, function (Faker $faker) {
+    return [
+        // Patient data
+        'address_1'         => $faker->streetAddress,
+        'address_2'         => $faker->randomElement(['', $faker->secondaryAddress]),
+        'city'              => $faker->city,
+        'state'             => $faker->state,
+        'zip'               => $faker->postcode,
+        'country'           => 'US',
+    ];
+});
